0d8a035c4f05c3f5d5f78844a8f40f8d3bd571b6
G-S1lk <metaxopoulos24@gmail.com>
2025-01-27 18:54:44 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <sys/socket.h> // AF_INET and SOCK_STREAM
 #include <arpa/inet.h> // storage size of serv_addr
@@ -13,65 +14,76 @@
 #define ORDERS 10
 
 //Global variables
-double total_profit = 0.0;
-int sucs_orders = 0;
-int failed_orders = 0;
-char Buffer[256] = { 0 };
+
 int random_item;
-double total_price; 
 int error_flag;
-int sucs_request;
-int failed_request;
 int random_item;
 
-void order_update(int e_f){
-  if (e_f == 0){
-    printf("Item %d purchase was successfull\n", random_item);
-  }
-  else{
-    printf("Item %d purchase was unsuccessfull\n", random_item);
-  }
-}
 
-
-int main(int argc, char const *argv[]) {
-  srand(time(NULL)); // Seed the random number generator once at the start
- 
-  
-  
+int Send_Order(int random_item){
   int sock = 0, valread;
   struct sockaddr_in serv_addr;
-  
+
   if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("\n Socket creation error \n");
     return -1; 
   }
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
-  
+
   if (inet_pton (AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
     printf("\nInvalid address/ Address not supported \n");
   return -1; 
   }
-  
+
   if (connect (sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
     printf("\nConnection Failed \n");
   return -1; 
-  }
-    for (int i ;i<ORDERS ;i++) {
-    sleep(1);
-      
-    random_item = rand() % (MAX_ITEMS); // Generate random item
-      
-    
-      
-    write(sock, &random_item, sizeof(int)); //Clinet sends the number of the wanted item
-      
-    read(sock,&error_flag, sizeof(int)); //Reads the response from the sever
-      
-    order_update(error_flag);
-    
     
   }
+  random_item = rand() % (MAX_ITEMS); // Generate random item
+  write(sock, &random_item, sizeof(int)); //stelnei o client ton ar tou item pou thelei
+  printf("Item requested: %d\n", random_item);
+  read(sock,&error_flag, sizeof(int)); //Reads the status of the order from server
+  if (error_flag == 0){
+    printf("Item %d purchase was successfull\n", random_item);
+  }
+  else{
+    printf("Item %d purchase was unsuccessfull\n", random_item);
+    exit(1);
+  }
+  close(sock);
+}
+
+
+
+int main(int argc, char const *argv[]) {
+  srand(time(NULL)); // Seed the random number generator once at the start
+
+  for (int i = 0; i < CLIENTS; i++){
+    printf("\n\nClient: %d\n\n", i+1);
+    for (int y=0 ;y<ORDERS ;y++) {
+      pid_t pid = fork();
+      random_item = rand() % (MAX_ITEMS); // Generate random item
+      if (pid == 0) {//Child process sends order to server
+          
+        Send_Order(random_item);
+        exit(0);
+      } 
+      else if (pid > 0) {//Parent process adds 1sec delay to the orders
+        sleep(1);
+      } 
+      else {
+        perror("Fork failed");
+        exit(1);
+      }
+    
+             
+   }
+  }
+  for (int i = 0; i < ORDERS; i++) {//Wait for process to finish
+      wait(NULL);
+  }
+  
 return 0;
 } 
