0fb6e794a1462ab99a07f493244fb1b54e0a0285
G-S1lk <metaxopoulos24@gmail.com>
2025-01-28 17:38:04 +0200

    Update client.c

--- client.c
+++ client.c
@@ -6,19 +6,17 @@
 #include <unistd.h> // getpid, getppid, fork
 #include <string.h> // bzero
 #include <time.h>
-#define PORT 8080
-//Constast variables
 
+//Constast variables
 #define CLIENTS 5       
 #define MAX_ITEMS 20
 #define ORDERS 10
+#define PORT 8080
 
 //Global variables
-
 int random_item;
 int error_flag;
 int random_item;
-
 
 int Send_Order(int random_item){
   int sock = 0, valread;
@@ -42,28 +40,25 @@ int Send_Order(int random_item){
     
   }
   random_item = rand() % (MAX_ITEMS); // Generate random item
-  write(sock, &random_item, sizeof(int)); //stelnei o client ton ar tou item pou thelei
-  printf("Item requested: %d\n", random_item);
+  write(sock, &random_item, sizeof(int)); //Client sends the nubmer of the item that he wants
+  printf("Number of item %d ,requested for purchase\n", random_item);
   read(sock,&error_flag, sizeof(int)); //Reads the status of the order from server
   if (error_flag == 0){
-    printf("Item %d purchase was successfull\n", random_item);
+    printf("Item %d purchase was successfull\n\n", random_item);
   }
   else{
-    printf("Item %d purchase was unsuccessfull\n", random_item);
+    printf("Item %d purchase was unsuccessfull\n\n", random_item);
     exit(1);
   }
   close(sock);
 }
-
-
 
 int main(int argc, char const *argv[]) {
   srand(time(NULL)); // Seed the random number generator once at the start
 
   for (int i = 0; i < CLIENTS; i++){
     printf("\n\nClient: %d\n\n", i+1);
-    int y = 0;
-   while (y<ORDERS && error_flag == 0) {
+    for (int y=0 ;y<ORDERS ;y++) {
       pid_t pid = fork();
       random_item = rand() % (MAX_ITEMS); // Generate random item
       if (pid == 0) {//Child process sends order to server
@@ -77,10 +72,8 @@ int main(int argc, char const *argv[]) {
       else {
         perror("Fork failed");
         exit(1);
-      }
-    
-       j++;      
-   }
+      }         
+    }
   }
   for (int i = 0; i < ORDERS; i++) {//Wait for process to finish
       wait(NULL);
