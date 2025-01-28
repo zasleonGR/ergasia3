32867bfacb401438972d4dec89770d20e34ce105
G-S1lk <metaxopoulos24@gmail.com>
2025-01-28 17:36:04 +0200

    Update eshop_server.c

--- eshop_server.c
+++ eshop_server.c
@@ -14,22 +14,22 @@
 double total_profit = 0.0;
 int sucs_orders = 0;
 int failed_orders = 0;
-int error_flag = 0;
-
 double total_price; 
+int error_flag;
 int sucs_request;
 int failed_request;
 int item_number;
+int not_complete_orders;
 
-void print_result(int Client, double Final_price, int error_flag) {
+void print_result(int Client, double Final_price, int flag,int n_c_o) {
     printf("Client %d:", Client);
-    if (error_flag == 0) {
-        printf(" Purchase complete, your total is: %.2f euro.\n", Final_price);
-        sucs_request++;
-        total_profit += Final_price; // Add to total_profit for this client
+    total_profit += Final_price; // Add to total_profit for this client
+    if (flag == 0 && n_c_o ==0) {
+        printf(" Purchase complete, your total is: %.2f euro.(Successful!!!)\n\n", Final_price);
+        sucs_request++;    
     } else {
-        printf(" At least 1 product was unavailable, request failed.\n");
-        failed_request++;
+        printf(" Purchase complete,%d products were unavailable, your total is: %.2f euro.(Failed!!!)\n\n", not_complete_orders,Final_price);
+        failed_request++;\  
     }
 }
 
@@ -38,7 +38,6 @@ void end_results_print(int s_o,int f_o,double t_p,int s_r,int f_r){
     printf("Total successful orders: %d\n", sucs_orders);
     printf("Total failed orders: %d\n", failed_orders);
     printf("Total profit: %.2f euro.\n\n", total_profit);
-
     printf("%d products were requested,where %d products were bought,totaling %.2f euros.\n", sucs_orders + failed_orders, sucs_orders, total_profit);
     printf("%d requests were made,where %d succeeded and %d failed\n", sucs_request+failed_request, sucs_request, failed_request);
 }
@@ -61,6 +60,7 @@ void initialize_catalog() {
 void initialize_prices() {
     for (int i = 0; i < MAX_ITEMS; i++) {
         catalog[i].price = rand() % (1000 + 1);
+    
     }
 }
 
@@ -73,23 +73,20 @@ int process_order(int item_number) {
         sucs_orders++;
     } else {
         error_flag = 1; // Order failed
+        not_complete_orders++;
         failed_orders++ ;  
     }
     return error_flag;
 }
 
 
-
-int main() {
-    
+int main() {  
     srand(time(NULL));
     int opt=1; //Used in line 109
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    
-    
-    
+ 
     initialize_catalog(); //We initalize the catalog (and the prices of the items etc)
     initialize_prices();
 
@@ -122,36 +119,29 @@ int main() {
         close(server_fd);
         exit(EXIT_FAILURE);
     }
-
-    printf("Server is listening on port 8080...\n\n");
-
+    
+    printf("Eshop is waiting for orders...\n\n");
+    
     for (int i = 0; i < MAX_CLIENTS; i++){
-        int j = 0;
-        while (j < ORDERS && error_flag == 0){
-            
+        printf("Client: %d\n",i+1);
+        for (int j = 0; j < ORDERS; j++){  
             // Accept connection
             if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
                 perror("Accept failed");
                 close(server_fd);
                 exit(EXIT_FAILURE);
             }
-            
             // Read and respond to client
-            read(new_socket, &item_number, sizeof(int)); //diabazoume ton arithmo proinots apo ton client
-            printf("Received item: %d\n", item_number);
+            read(new_socket, &item_number, sizeof(int)); //Server reads the item number the client requested
+            printf("Received item number %d for order\n", item_number);
             process_order(item_number);
-           
-            write(new_socket, &error_flag, sizeof(int));//stellnoume apantisi 
-           
-            close(new_socket);
-            j++;
+            write(new_socket, &error_flag, sizeof(int));//Server sends the result of the order   
+        close(new_socket);
         }
-        print_result(i+1, total_price, error_flag);
-        
-    }
-
+        print_result(i+1, total_price, error_flag,not_complete_orders);
+        not_complete_orders=0;
+    } 
     end_results_print(sucs_orders, failed_orders, total_profit, sucs_request, failed_request);
-    
     
     return 0;
 }
