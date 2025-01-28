7d93d281433d8b6a773104912e4af4d51d962bca
zasleonGR <zachariasleonis@gmail.com>
2025-01-28 17:58:15 +0200

    Update eshopFunctions.h

--- eshopFunctions.h
+++ eshopFunctions.h
@@ -72,3 +72,38 @@ int process_order(int item_number) {
     }
     return error_flag;
 }
+
+int Send_Order(int random_item){
+  int sock = 0, valread;
+  struct sockaddr_in serv_addr;
+
+  if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("\n Socket creation error \n");
+    return -1; 
+  }
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  if (inet_pton (AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+    printf("\nInvalid address/ Address not supported \n");
+  return -1; 
+  }
+
+  if (connect (sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    printf("\nConnection Failed \n");
+  return -1; 
+    
+  }
+  random_item = rand() % (MAX_ITEMS); // Generate random item
+  write(sock, &random_item, sizeof(int)); //Client sends the nubmer of the item that he wants
+  printf("Number of item %d ,requested for purchase\n", random_item);
+  read(sock,&error_flag, sizeof(int)); //Reads the status of the order from server
+  if (error_flag == 0){
+    printf("Item %d purchase was successfull\n\n", random_item);
+  }
+  else{
+    printf("Item %d purchase was unsuccessfull\n\n", random_item);
+    exit(1);
+  }
+  close(sock);
+}
